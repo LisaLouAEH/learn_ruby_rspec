73327dfca2ca90f6dfa70ae89b973f76e1c0230e
Lisa Lou <lisa.lou.aeh@gmail.com>
2018-10-08 17:00:20 +0200

    ex 00 à 03

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,27 @@
 #write your code here
+def add(a, b)
+    return a + b
+end
+
+def subtract(a, b)
+    return a - b
+end
+
+def sum(tab)
+    return tab.sum
+end
+
+def multiply(a, b)
+    return a * b
+end
+
+def power(a, b)
+    return a**b
+end
+
+def factorial(n)
+    if n == 0
+        return 1
+    end
+    return (1..n).inject(:*) || 1
+end
--- 03_basics/basics.rb
+++ 03_basics/basics.rb
@@ -1,1 +1,26 @@
 # write your code here
+def who_is_bigger(a, b, c)
+    hash = {
+    :a => a,
+    :b => b,
+    :c => c}
+    if a == nil || b == nil || c == nil
+        return "nil detected"
+    else
+        return "#{hash.key(hash.values.max)} is bigger"
+    end
+end
+
+def reverse_upcase_noLTA(string)
+    return string.upcase.reverse.delete("LTA")
+end
+
+def array_42(tab)
+    return tab.include? 42
+end
+
+def magic_array(tab)
+   return ((tab.flatten.sort).map{|n| n*2}).delete_if{|n| n % 3 == 0}.uniq
+end
+
+puts magic_array([5, 2, 3, [4, 1, 1])
