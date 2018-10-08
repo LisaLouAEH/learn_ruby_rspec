9bcc82f2eb0d8cbe231fa66ebc05c9e9a9e2b9a5
Lisa Lou <lisa.lou.aeh@gmail.com>
2018-10-08 22:54:07 +0200

    premiere partie du 06

--- 06_pig_latin/pig_latin.rb
+++ 06_pig_latin/pig_latin.rb
@@ -1,14 +1,15 @@
 #write your code here
-def pig_latin(string)
+def translate(string)
     back = string.size
     
     if string[0] == "a" || string[back] == "i" || string[back] == "e" || string[back] == "o" || string[back] == "y"
         string = string + "ay"
     else
         string_2 = string + string[0] + "ay"
-        return string_2[0]
+        string_2[0] = ""
+        return string_2
     end
     
     return string
 end
-puts pig_latin("banana")
+puts translate("apple")
