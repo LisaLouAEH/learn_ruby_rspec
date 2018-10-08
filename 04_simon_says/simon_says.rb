0ca498acd0d8027d804a04e0c1ed451ad7a36a50
Lisa Lou <lisa.lou.aeh@gmail.com>
2018-10-08 18:21:18 +0200

    ex 04

--- 04_simon_says/simon_says.rb
+++ 04_simon_says/simon_says.rb
@@ -1,1 +1,32 @@
 #write your code here
+def echo(string)
+    return string
+end
+
+def shout(string)
+    return string.upcase
+end
+
+def repeat(string, nb=2)
+    return [string] * nb * " "
+end
+
+def start_of_word(string, n)
+    if n == 1
+        return string[0]
+    else
+        return string[0..n-1]
+    end
+end
+
+def first_word(string)
+    n = 0
+    string.each do |char|
+        unless char == " "
+            n += 1
+        end
+        
+    end
+    return string[0..n]
+end
+puts first_word("hoogeek house")
